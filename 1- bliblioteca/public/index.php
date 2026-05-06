08e39fb56703fa7e405cec25b952ed2dc2eb7cf0
joaovitorper <jovipepa.10@gmail.com>
2026-05-06 08:14:49 -0300

    atualição de hoje de manha

--- 1- bliblioteca/public/index.php
+++ 1- bliblioteca/public/index.php
@@ -19,50 +19,65 @@ $livros = ler_livros();
     <a class="btn btn-primary" href="create.php"> + Novo Livro </a>
 </header>
 
-<div class= "card">
+<div class="card">
   <strong>Total de Livros:</strong> <?php echo count($livros); ?>
 </div>
 
 <?php if (count($livros) == 0): ?>
-  <div class="alerta">
-    Ainda não existe livros cadastrados.Clique em <strong>"+Novo Livro"</strong>
-</div>
+  <div class="alert"> 
+    Ainda não existe livros cadastrados. Clique em <strong>"+Novo Livro"</strong>
+  </div>
 <?php else:?>
 
-    <table>
-     <tr>
-    <tr>Titulo</tr>
-    <tr>Autor</tr>
-    <tr>ISBN</tr>
-    <tr>ano</tr>
+<table>
+
+
+  <thead>
+    <tr>
+      <th>Título</th>
+      <th>Autor</th>
+      <th>ISBN</th>
+      <th>Ano</th>
+   
     </tr>
-    
+  </thead>
+
+  <tbody>
     <?php foreach ($livros as $l):?>
     <tr>
         <td><?php echo e($l['titulo'])?></td>
         <td><?php echo e($l['autor'])?></td>
         <td><?php echo e($l['isbn'])?></td>
-        <td><span class="badge badge-ano"><?php echo e($l['ano']); ?></span></td>
+        <td>
+          <span class="badge badge-ano">
+            <?php echo e($l['ano']); ?>
+          </span>
+        </td>
 
         <td class="acoes">
-        <a class="btn btn-primary" href="update.php?id=<?php echo $l['id']; ?>">
-        Editar
-    </a>
-    <form action="delete.php" method="POST" style="display:inline;">
-    <input type="hidden" name="id" value="<?= htmlspecialchars($l['id']) ?>">
-    
-    <button type="submit" class="btn btn-danger"
-        onclick="return confirm('Excluir este livro?');">
-        Excluir
-    </button>
-</form>
-</td>
+          <a class="btn btn-primary" href="update.php?id=<?php echo $l['id']; ?>">
+            Editar
+          </a>
 
+          <form action="delete.php" method="POST" style="display:inline;">
+            <input type="hidden" name="id" value="<?= htmlspecialchars($l['id']) ?>">
+            
+            <button type="submit" class="btn btn-secondary"
+              onclick="return confirm('Excluir este livro?');">
+              Excluir
+            </button>
+          </form>
+        </td>
     </tr>
     <?php endforeach;?>
-    </table>
-    <?php endif; ?>
-    <p class="footer">Dados Guardados em <code> data/livros.json</code></p>
+  </tbody>
+
+</table>
+
+<?php endif; ?>
+
+<p class="footer">Dados Guardados em <code>data/livros.json</code></p>
+
 </div>
 </body>
 </html>
